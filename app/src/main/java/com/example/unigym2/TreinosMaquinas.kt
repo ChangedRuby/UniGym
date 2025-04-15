eb7b280e56c5cdbe2a8606a086732ece40390637
Marina Santiago <marinasantiago1718@gmail.com>
2025-04-15 16:58:41 -0300

    Tela de maquinas do Personal (sem botao de add)

--- app/src/main/java/com/example/unigym2/TreinosMaquinas.kt
+++ app/src/main/java/com/example/unigym2/TreinosMaquinas.kt
@@ -1,5 +1,6 @@
 package com.example.unigym2
 
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -7,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.SearchView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -23,12 +25,16 @@ class TreinosMaquinas : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+//        val searchView = findViewById<SearchView>(R.id.MaquinasSearchView)
+//        val searchPlate = searchView.findViewById<View>(androidx.appcompat.R.id.search_plate)
+//        searchPlate.setBackgroundColor(Color.parseColor("#3372788C"))
     }
 
     override fun onCreateView(
