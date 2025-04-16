024ff936b822e3df9a7e6324ba133ba193ef1a25
ChangedRuby <gugasboy7@gmail.com>
2025-04-16 10:23:37 -0300

    Added new fragment_treinos_ver_ficha_personal.xml

--- app/src/main/java/com/example/unigym2/TreinosVerFichaPersonal.kt
+++ app/src/main/java/com/example/unigym2/TreinosVerFichaPersonal.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -14,16 +13,13 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [TreinosPersonalTrainer.newInstance] factory method to
+ * Use the [TreinosVerFichaPersonal.newInstance] factory method to
  * create an instance of this fragment.
  */
-class TreinosPersonalTrainer : Fragment() {
+class TreinosVerFichaPersonal : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
-    lateinit var verTreinosBtn: Button
-    private lateinit var communicator: Communicator
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,16 +34,7 @@ class TreinosPersonalTrainer : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        var v = inflater.inflate(R.layout.fragment_treinos_personal_trainer, container, false)
-
-        verTreinosBtn = v.findViewById(R.id.viewTreinosPersonal)
-
-        verTreinosBtn.setOnClickListener {
-            communicator = activity as Communicator
-            communicator.replaceFragment(TreinosVerFichaPersonal())
-        }
-
-        return v
+        return inflater.inflate(R.layout.fragment_treinos_ver_ficha_personal, container, false)
     }
 
     companion object {
@@ -57,12 +44,12 @@ class TreinosPersonalTrainer : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment TreinosPersonalTrainer.
+         * @return A new instance of fragment TreinosViewFichaPersonal.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            TreinosPersonalTrainer().apply {
+            TreinosVerFichaPersonal().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
