6897953ae6c81b117760113b0556d945e6488635
gsaraiva2109 <gsaraiva2109@gmail.com>
2025-04-18 21:48:06 -0300

    Add EditProfilePersonal fragment and update ProfilePersonal for editing functionality

---
+++ app/src/main/java/com/example/unigym2/Fragments/Profile/EditProfilePersonal.kt
@@ -0,0 +1,86 @@
+package com.example.unigym2.Fragments.Profile
+
+    import android.os.Bundle
+    import android.util.Log
+    import android.view.LayoutInflater
+    import android.view.View
+    import android.view.ViewGroup
+    import android.widget.Button
+    import androidx.fragment.app.Fragment
+    import com.example.unigym2.Activities.Communicator
+    import com.example.unigym2.R
+    import com.google.android.material.textfield.TextInputEditText
+
+    class EditProfilePersonal : Fragment() {
+        private lateinit var communicator: Communicator
+        private lateinit var saveButton: Button
+        private lateinit var usernameEditText: TextInputEditText
+        private lateinit var specialtyET1: TextInputEditText
+        private lateinit var specialtyET2: TextInputEditText
+        private lateinit var specialtyET3: TextInputEditText
+        private lateinit var specialtyET4: TextInputEditText
+        private lateinit var serviceNameET1: TextInputEditText
+        private lateinit var serviceNameET2: TextInputEditText
+        private lateinit var serviceNameET3: TextInputEditText
+        private lateinit var serviceNameET4: TextInputEditText
+        private lateinit var servicePriceET1: TextInputEditText
+        private lateinit var servicePriceET2: TextInputEditText
+        private lateinit var servicePriceET3: TextInputEditText
+        private lateinit var servicePriceET4: TextInputEditText
+
+        override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+            val view = inflater.inflate(R.layout.fragment_edit_profile_personal, container, false)
+
+            // Initialize communicator
+            communicator = activity as Communicator
+
+            // Initialize UI elements
+            usernameEditText = view.findViewById(R.id.editTextUsername)
+            specialtyET1 = view.findViewById(R.id.editText1)
+            specialtyET2 = view.findViewById(R.id.editText2)
+            specialtyET3 = view.findViewById(R.id.editText3)
+            specialtyET4 = view.findViewById(R.id.editText4)
+            serviceNameET1 = view.findViewById(R.id.editText5)
+            serviceNameET2 = view.findViewById(R.id.editText6)
+            serviceNameET3 = view.findViewById(R.id.editText7)
+            serviceNameET4 = view.findViewById(R.id.editText8)
+            servicePriceET1 = view.findViewById(R.id.editText9)
+            servicePriceET2 = view.findViewById(R.id.editText10)
+            servicePriceET3 = view.findViewById(R.id.editText11)
+            servicePriceET4 = view.findViewById(R.id.editText12)
+
+            // Add Save Button functionality (you'll need to add this button to your layout)
+            saveButton = view.findViewById(R.id.saveButton)
+            saveButton.setOnClickListener {
+                saveProfileChanges()
+                communicator.replaceFragment(ProfilePersonal())
+                Log.d("personalLog", "Profile Saved")
+            }
+
+            return view
+        }
+
+        private fun saveProfileChanges() {
+            // Get values from edit texts
+            val username = usernameEditText.text.toString()
+            val specialty1 = specialtyET1.text.toString()
+            val specialty2 = specialtyET2.text.toString()
+            val specialty3 = specialtyET3.text.toString()
+            val specialty4 = specialtyET4.text.toString()
+            val service1 = serviceNameET1.text.toString()
+            val service2 = serviceNameET2.text.toString()
+            val service3 = serviceNameET3.text.toString()
+            val service4 = serviceNameET4.text.toString()
+            val servicePrice1 = servicePriceET1.text.toString()
+            val servicePrice2 = servicePriceET2.text.toString()
+            val servicePrice3 = servicePriceET3.text.toString()
+            val servicePrice4 = servicePriceET4.text.toString()
+
+            // TODO: Implement saving data to your database or storage
+            // For example: FirebaseDatabase.getInstance().getReference("users").child(userId).setValue(userProfile)
+        }
+    }
---
+++ app/src/main/java/com/example/unigym2/Fragments/Profile/ProfilePersonal.kt
@@ -0,0 +1,45 @@
+package com.example.unigym2.Fragments.Profile
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.example.unigym2.Activities.Communicator
+import com.example.unigym2.R
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+class ProfilePersonal : Fragment() {
+    private var param1: String? = null
+    private var param2: String? = null
+
+    lateinit var editBtn : Button
+    private lateinit var communicator : Communicator
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        var v = inflater.inflate(R.layout.fragment_profile_personal, container, false)
+        communicator = activity as Communicator
+        editBtn = v.findViewById(R.id.editButton)
+        editBtn.setOnClickListener {
+            communicator.replaceFragment(EditProfilePersonal())
+            Log.d("personalLog", "Clicked")
+        }
+        return v
+    }
+}
