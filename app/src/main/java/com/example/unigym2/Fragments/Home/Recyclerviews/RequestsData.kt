4deb951c666036b50df273b7b7dcd07b1299ff59
Marina Santiago <marinasantiago1718@gmail.com>
2025-05-17 17:22:03 -0300

    Solicitações personal

--- app/src/main/java/com/example/unigym2/Fragments/Home/Recyclerviews/RequestsData.kt
+++ app/src/main/java/com/example/unigym2/Fragments/Home/Recyclerviews/RequestsData.kt
@@ -1,3 +1,11 @@
 package com.example.unigym2.Fragments.Home.Recyclerviews
 
-data class RequestsData(var name: String ?= null)
+data class RequestsData(
+    val nomeCliente: String? = null,
+    val data: String? = null,
+    val hora: String? = null,
+    val servico: String? = null,
+    val clienteID: String? = null,
+    val personalID: String? = null,
+    val agendamentoID: String? = null
+)
