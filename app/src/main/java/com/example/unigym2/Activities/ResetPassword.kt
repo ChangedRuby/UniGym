a2b491884eb812e0c6dc7c2cfb27e48995f153ed
manuol14 <manupimentelrg@edu.unifor.br>
2025-05-22 19:22:49 -0300

    Ajeitei a parte de alterar senha pelo email

--- app/src/main/java/com/example/unigym2/Activities/ResetPassword.kt
+++ app/src/main/java/com/example/unigym2/Activities/ResetPassword.kt
@@ -13,8 +13,6 @@ import com.google.firebase.auth.FirebaseAuth
 class ResetPassword : AppCompatActivity() {
 
     lateinit var emailField : TextInputEditText
-    lateinit var newPassawordField : TextInputEditText
-    lateinit var newConfirmsPassawordField: TextInputEditText
     lateinit var newPasswordButton: Button
     lateinit var auth: FirebaseAuth
 
@@ -24,60 +22,36 @@ class ResetPassword : AppCompatActivity() {
         setContentView(R.layout.activity_reset_password)
 
         emailField = findViewById(R.id.emailEditText)
-        newPassawordField = findViewById(R.id.passwordResetEditText)
-        newConfirmsPassawordField = findViewById(R.id.confirmsPassawordResetEditText)
         newPasswordButton = findViewById(R.id.confirmChangesBtn)
         auth = FirebaseAuth.getInstance()
     }
 
     override fun onStart() {
         super.onStart()
-
         newPasswordButton.setOnClickListener {
-            val inputEmail = emailField.text.toString().trim()
-            val newPassword = newPassawordField.text.toString().trim()
-            val newConfirmsPassword = newConfirmsPassawordField.text.toString().trim()
+            val email = emailField.text.toString().trim()
 
-            if(inputEmail.isEmpty() ||newPassword.isEmpty() || newConfirmsPassword.isEmpty()){
-                Toast.makeText(this, "Preencha todos os campos.", Toast.LENGTH_SHORT).show()
+            if(email.isEmpty()){
+                emailField.error = "Insira o e-mail"
                 return@setOnClickListener
             }
 
-            if(newPassword != newConfirmsPassword) {
-                Toast.makeText(this, "As senhas não coincidem.", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }
+            auth.sendPasswordResetEmail(email)
+                .addOnCompleteListener{ task ->
+                    if(task.isSuccessful){
+                        Toast.makeText(this,"Link de redefinição de senha enviado para: $email",
+                            Toast.LENGTH_LONG).show()
 
-            val user = auth.currentUser
-            if( user != null){
-                val userEmail = user.email
-                if (userEmail == inputEmail) {
-                    user.updatePassword(newPassword)
-                        .addOnCompleteListener { task ->
-                            if (task.isSuccessful) {
-                                Toast.makeText(
-                                    this,
-                                    "Senha alterada com sucesso!",
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                                val intent = Intent(this, LoginMenu::class.java)
-                                intent.putExtra("senha_alterada", true)
-                                startActivity(intent)
-                                finish()
-                            } else {
-                                Toast.makeText(
-                                    this, "Erroa ao alterar senha: ${task.exception?.message}",
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                            }
+                        val intent = Intent(this, LoginMenu::class.java)
+                        intent.putExtra("senha_alterdada", true)
+                        startActivity(intent)
+                        finish()
+                    }
+                        else{
+                        Toast.makeText(this,"Erro ao enviar link: ${task.exception?.message}", Toast.LENGTH_LONG).show()
                         }
-                } else {
-                Toast.makeText(this, "O e-mail informado não corresponde ao do usuário logado.", Toast.LENGTH_SHORT).show()
-            }
+                }
 
-            } else {
-                Toast.makeText(this, "Usuário não autenticado.", Toast.LENGTH_SHORT).show()
-            }
         }
 
     }
